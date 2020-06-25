dbf7a02d5d0ec29bd202dbc3b2cae348a3b7eb57
ssk <sasat.s.93@gmail.com>
2020-06-25 21:46:39 +0900

    figure非表示で全画面表示グラフ保存成功 figure非表示：make_fig_off figure表示：make_fig_on
    h.WindowState = 'maximized' では、非表示にしたとき
    デフォルトサイズのグラフが保存される

---
+++ make_fig_on.m
@@ -0,0 +1,10 @@
+clear all
+close all
+y=1:10;
+% h = figure('visible','off');
+h = figure('visible','on','units', 'normalized', 'outerposition', [0 0 1 1]);
+
+
+plot(y);
+% h.WindowState = 'maximized';
+saveas(h,'fig.png');
